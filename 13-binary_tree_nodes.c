7b93b16e63318bc6ccc471a76310f12287ae8f97
AerinLom <aerinlom1@gmail.com>
2024-03-26 12:10:27 -0700

    Fixed task 13 output

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,17 +8,21 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
+	size_t num_nodes = 0;
 
 
 	if (tree == NULL)
 	{
-		return (nodes);
+		return (num_nodes);
 	}
 
-	nodes += 1;
-	nodes += binary_tree_nodes(tree->left);
-	nodes += binary_tree_nodes(tree->right);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+	num_nodes += 1;
+	}
 
-	return (nodes);
+	num_nodes += binary_tree_nodes(tree->left);
+	num_nodes += binary_tree_nodes(tree->right);
+
+	return (num_nodes);
 }
